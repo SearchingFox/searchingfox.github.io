66637e92922e0b219d4c98e039b4d5b2dd75f6f5
SearchingFox <n3tstrang3r@yandex.ru>
2026-05-09 17:49:06 +0300

    Update deploy

--- digital-garden/quartz/components/Head.tsx
+++ digital-garden/quartz/components/Head.tsx
@@ -4,7 +4,7 @@ import { CSSResourceToStyleElement, JSResourceToScriptElement } from "../util/re
 import { googleFontHref, googleFontSubsetHref } from "../util/theme"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { unescapeHTML } from "../util/escape"
-// import { CustomOgImagesEmitterName } from "../../.quartz/plugins"
+import { CustomOgImagesEmitterName } from "../../.quartz/plugins"
 export default (() => {
   const Head: QuartzComponent = ({
     cfg,
@@ -31,10 +31,10 @@ export default (() => {
     const socialUrl =
       fileData.slug === "404" ? url.toString() : joinSegments(url.toString(), fileData.slug!)
 
-    // const usesCustomOgImage = ctx.cfg.plugins.emitters.some(
-    //   (e) => e.name === CustomOgImagesEmitterName,
-    // )
-    // const ogImageDefaultPath = `https://${cfg.baseUrl}/static/og-image.png`
+    const usesCustomOgImage = ctx.cfg.plugins.emitters.some(
+      (e) => e.name === CustomOgImagesEmitterName,
+    )
+    const ogImageDefaultPath = `https://${cfg.baseUrl}/static/og-image.png`
 
     return (
       <head>
@@ -61,6 +61,18 @@ export default (() => {
         <meta name="twitter:description" content={description} />
         <meta property="og:description" content={description} />
         <meta property="og:image:alt" content={description} />
+
+        {!usesCustomOgImage && (
+          <>
+            <meta property="og:image" content={ogImageDefaultPath} />
+            <meta property="og:image:url" content={ogImageDefaultPath} />
+            <meta name="twitter:image" content={ogImageDefaultPath} />
+            <meta
+              property="og:image:type"
+              content={`image/${getFileExtension(ogImageDefaultPath) ?? "png"}`}
+            />
+          </>
+        )}
 
         {cfg.baseUrl && (
           <>
